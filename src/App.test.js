59e1c90b6a56e2626bdfd724686b4a6822bb361a
Eduardo Moura <morphex.e@gmail.com>
2018-03-27 10:36:41 +0100

    #Moved camelcase rule to warning #Changed test suit to fully run on commit #Fixed App.test to load the store and provider

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,21 @@ import App from "./components/App";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import entryApp from "./reducers/reducers";
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(entryApp);
+
+beforeAll(() => {});
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
