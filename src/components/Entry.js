9c100596b5d13fb9efa486d69618a0264749a252
Ian Mayo <ian@planetmayo.com>
2018-03-27 22:27:08 +0100

    minor whitespace change, to trick linter

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -4,28 +4,24 @@ import { withState } from "recompose";
 import PropTypes from "prop-types";
 import React from "react";
 
-const Entry = ({
-  selected,
-  entry: { text, id, created, mType, privacy }
-}) => (
+const Entry = ({ selected, entry: { text, id, created, mType, privacy } }) => (
   <div
     style={{
-        backgroundColor: selected ? "#FFFFFF" : "transparent"
-      }}>
+      backgroundColor: selected ? "#FFFFFF" : "transparent"
+    }}
+  >
     <span>
-      {created.toLocaleTimeString()}  {id}
-      <Badge style={{ margin:"2px",width: "100px" }}>{mType}</Badge>
-      <Badge>
-        {privacy}
-      </Badge>
+      {created.toLocaleTimeString()} {id}
+      <Badge style={{ margin: "2px", width: "100px" }}>{mType}</Badge>
+      <Badge>{privacy}</Badge>
     </span>
 
     <span style={{ paddingLeft: "10px" }}>
       {Object.keys(text).map(key => (
         <span style={{ paddingLeft: "5px" }}>
-          <b>{key}</b> : {text[key]}
+          <b>{key}</b>: {text[key]}
         </span>
-        ))}
+      ))}
     </span>
   </div>
 );
