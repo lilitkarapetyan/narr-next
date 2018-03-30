89a8177d9da013bf7c7c7201f7c93b41f696b391
bhumi shah <shahbhumi53@gmail.com>
2018-03-30 17:31:53 +0530

    completed issue #72 and #73

--- src/components/RightPanel/RightPanel.js
+++ src/components/RightPanel/RightPanel.js
@@ -3,7 +3,6 @@ import { CategoryType } from "../Schemas";
 import { Row } from "reactstrap";
 import { branch, renderComponent } from "recompose";
 import CategoryRender from "./CategoryRender";
-import NavigationPanel from "./NavigationPanel";
 import PropTypes from "prop-types";
 import React from "react";
 import SettingsCategory from "./SettingsCategory";
@@ -23,7 +22,6 @@ const RightPanel = ({ categories, collapse, addEntry }) => (
         />
       ))}
       <SettingsCategory />
-      <NavigationPanel speed={100} angle={80} />
     </Row>
     <div className="category-count text-center">
       Categories Count {categories.length}
