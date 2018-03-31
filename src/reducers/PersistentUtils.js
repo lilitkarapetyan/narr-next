76f55c41614b55699a60074bbff1fa98daac3903
Eduardo Moura <morphex.e@gmail.com>
2018-03-31 13:56:07 +0100

    Fixed #93 Bulk Entry Generation ReAdded PouchDB

--- src/reducers/PersistentUtils.js
+++ src/reducers/PersistentUtils.js
@@ -1,27 +1,26 @@
 import { persistentReducer } from "redux-pouchdb-plus";
 
-const Logging = () => ({
-  onInit: (reducerName, reducerState) => {
-    console.log(`INITIALIZED :: ${reducerName}`, reducerState);
-    // Called when this reducer was initialized
-    // (the state was loaded from or saved to the
-    // database for the first time or after a reinit action).
-  },
-  onUpdate: (reducerName, reducerState) => {
-    console.log(`UPDATED :: ${reducerName}`, reducerState);
+// const Logging = () => ({
+//   onInit: (reducerName, reducerState) => {
+//     console.log(`INITIALIZED :: ${reducerName}`, reducerState);
+//     // Called when this reducer was initialized
+//     // (the state was loaded from or saved to the
+//     // database for the first time or after a reinit action).
+//   },
+//   onUpdate: (reducerName, reducerState) => {
+//     console.log(`UPDATED :: ${reducerName}`, reducerState);
 
-    // Called when the state of reducer was updated with
-    // data from the database.
-    // Cave! The store still contains the state before
-    // the updated reducer state was applied to it.
-  },
-  onSave: (reducerName, reducerState) => {
-    console.log(`SAVED :: ${reducerName}`, reducerState);
+//     // Called when the state of reducer was updated with
+//     // data from the database.
+//     // Cave! The store still contains the state before
+//     // the updated reducer state was applied to it.
+//   },
+//   onSave: (reducerName, reducerState) => {
+//     console.log(`SAVED :: ${reducerName}`, reducerState);
 
-    // Called every time the state of this reducer was
-    // saved to the database.
-  }
-});
+//     // Called every time the state of this reducer was
+//     // saved to the database.
+//   }
+// });
 
-export default (reducer, log = false) =>
-  persistentReducer(reducer, log && Logging());
+export default (reducer, name) => persistentReducer(reducer, { name });
