4ea871a7a00821f4003033596940d5a597530b0d
bhumi shah <shahbhumi53@gmail.com>
2018-03-31 15:04:42 +0530

    fixed errors in test cases

--- src/reducers/Selectors/listenEvent.js
+++ src/reducers/Selectors/listenEvent.js
@@ -1,19 +1,13 @@
 import { createSelector } from "reselect";
-import { validEntries } from "./entrySelectors";
+import { getEntries } from "./entrySelectors";
 import moment from "moment";
 
 export const listenEvent = type =>
-  createSelector([validEntries], entries => {
-    let filtered = [];
-    if (typeof type === "string") {
-      filtered = entries.filter(enty => enty.mType === type);
-    } else {
-      filtered = entries.filter(type);
-    }
+  createSelector([getEntries], entries => {
+    const filtered = entries.filter(enty => enty.mType === type);
     const sorted = filtered.sort((x, y) =>
       moment(x.created).isAfter(moment(y.created) ? 1 : -1)
     );
     if (sorted.length > 0) return sorted[0];
-
-    return filtered;
+    return null;
   });
